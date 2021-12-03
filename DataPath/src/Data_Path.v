d02ccd22c561050dbb32a762a7778e567bb70007
Alexis Lopez <alexis.sbtg@gmail.com>
2021-12-03 14:01:43 -0600

    Test in progress

--- DataPath/src/Data_Path.v
+++ DataPath/src/Data_Path.v
@@ -1,4 +1,11 @@
 module Data_Path(
+    // Inputs
+    input clk,
+    input reset,
+    //Output
+    output [7:0] GPIO_o,
+
+    /********* Señales de Control *********/
     input PCWrite,
           IorD,
           MemWrite,
@@ -12,9 +19,20 @@ module Data_Path(
     input PCSrc,
           PCWrite
 );
-    wire PC_i, PC_o;
+    wire [31:0] PC_i_w, PC_o_w;
+    wire [31:0] Adr_w;
+    wire [31:0] SrcA_w, SrcB_w;
+    wire [31:0] ALUResult_w, ALUOut_w;
 
-    Reg_Simple PROGRAM_COUNTER(.clk(clk), .data_i(PC_i), .data_o(PC_o));
+    //PC
+    Reg_Simple PROGRAM_COUNTER(.clk(clk), .data_i(PC_i_w), .data_o(PC_o_w));
+    //Adr
+    Mux_2x1 Adr(.in_0(PC_o_w), .in_1(ALUOut_w), .data_out(Adr_w));
+    //Mem_out RD
+
+    Mux_2x1 SrcA(.in_0(PC_o_w), .in_1(/**/), .data_out(SrcA_w));
+    ALU ALUIF(.selector(ALUControl), .a(SrcA_w), .b(SrcB_w), .data_out(ALUResult_w));
+
 
 
 
